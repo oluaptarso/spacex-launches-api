526cdb4025b1b82cdb29d133402b52a1dd6e475e
oluaptarso <oluaptarsso@gmail.com>
2023-03-17 15:47:47 -0300

    chore(app): configure ConfigModule with validation, and CacheModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import * as Joi from 'joi';
+import { SpacexAPIModule } from './spacex-api/spacex-api.module';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        SPACEX_API_URL: Joi.string().required(),
+      }),
+      envFilePath: '.env',
+    }),
+    SpacexAPIModule,
+    CacheModule.register({
+      ttl: 1000 * 5, // milliseconds
+      max: 10, // maximum number of items in cache
+      isGlobal: true,
+    }),
+  ],
+  controllers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
