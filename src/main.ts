486125a5cb525b66ca7e5626d1b3e253941f0b9b
oluaptarso <oluaptarsso@gmail.com>
2023-03-17 15:48:21 -0300

    chore(main.ts): configure FormatResponseFilter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FormatResponseFilter } from './format-response/filters/format-response.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new FormatResponseFilter(httpAdapter));
+
   await app.listen(3001);
 }
 bootstrap();
